e421a8b26cc998e39fbe61dbad1e061ff0ac6ecb
Hasindu Shanuka <hasindur@digital400.com>
2023-10-18 18:00:23 +0530

    Create useSignOut hook

---
+++ hooks/useSignOut.ts
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/navigation'
+import { useDispatch } from 'react-redux'
+// import secureLocalStorage from 'react-secure-storage'
+import { clientInstance } from '@/lib/axios'
+import { deleteSession } from '@/context/session/sessionSlice'
+
+export default function useSignOut() {
+  const dispatch = useDispatch()
+  const router = useRouter()
+
+  return async () => {
+    const { status } = await clientInstance.post('/auth/sign-out')
+
+    if (status === 201) {
+      dispatch(deleteSession())
+      localStorage.clear()
+      sessionStorage.clear()
+      router.push('/')
+    }
+  }
+}
