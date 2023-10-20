e078a4deb80bfefdeda2700a22f7d11c39114cc4
Hasindu Shanuka <hasindur@digital400.com>
2023-10-20 11:56:41 +0530

    Add api private paths to matcher

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,10 @@ export function middleware(request: NextRequest) {
     const response = NextResponse.next()
     return response
   } else {
-    return NextResponse.redirect(new URL(process.env.CLIENT_BASE_URL as string))
+    return NextResponse.redirect(new URL('http://localhost:3000/'))
   }
 }
 
 export const config = {
-  matcher: '/private/:path*',
+  matcher: ['/private/:path*', '/api/auth/:path*'],
 }
