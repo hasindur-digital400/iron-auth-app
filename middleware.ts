d07c12585ec75066862f4f422de8d2512ba479dc
Hasindu Shanuka <hasindur@digital400.com>
2023-10-21 20:02:20 +0530

    Remove sign-in path from middleware

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   if (request.cookies.has('iron_session_cookie')) {
-    if (request.nextUrl.pathname.startsWith('/private')) {
-      const response = NextResponse.next()
-      return response
-    } else if (request.nextUrl.pathname.startsWith('/auth/sign-in')) {
-      return NextResponse.redirect(new URL('http://localhost:3000/'))
-    }
+    const response = NextResponse.next()
+    return response
   } else {
     return NextResponse.redirect(new URL('http://localhost:3000/'))
   }
+}
+
+export const config = {
+  matcher: ['/private/:path*'],
 }
