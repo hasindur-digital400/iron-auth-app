5bb14498c8b61d5423cd1267885828a3705571c3
Hasindu Shanuka <hasindur@digital400.com>
2023-10-21 19:05:39 +0530

    Include sign-in path in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   if (request.cookies.has('iron_session_cookie')) {
-    const response = NextResponse.next()
-    return response
+    if (request.nextUrl.pathname.startsWith('/private')) {
+      const response = NextResponse.next()
+      return response
+    } else if (request.nextUrl.pathname.startsWith('/auth/sign-in')) {
+      return NextResponse.redirect(new URL('http://localhost:3000/'))
+    }
   } else {
     return NextResponse.redirect(new URL('http://localhost:3000/'))
   }
-}
-
-export const config = {
-  matcher: ['/private/:path*', '/api/auth/:path*'],
 }
